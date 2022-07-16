3559ba5557307a2c3a2cd94a86cc2aab551a1ce5
Vimpandra <victorimperatriz@gmail.com>
2022-07-16 19:53:39 -0300

    added a comment with the mistake I made

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const container = document.querySelector(`.container`);
 
+// function needs to return the square number of N
 function createSquares(n) {
     for (let i = 1; i <= n; i++) {
         let square = document.createElement(`div`);
