33d34be46dbba091a1e7c19c1c329a3198930ac7
Vimpandra <victorimperatriz@gmail.com>
2022-07-19 03:31:40 -0300

    Added a size slider selector with functionality

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 // Drag and drop event disabler
-const div = document.querySelector('div')
-div.addEventListener('dragstart', (e) => {
+const container = document.querySelector(`#container`);
+container.addEventListener('dragstart', (e) => {
   e.preventDefault()
-})
-
-div.addEventListener('drop', (e) => {
+});
+container.addEventListener('drop', (e) => {
   e.preventDefault()
-})
+});
 //------------------------------------------
 
-const container = document.querySelector(`.container`);
+
+const sizePara = document.getElementById(`sizePara`);
+const sizeSlider = document.getElementById(`sizeSlider`);
+
+sizePara.textContent = `${sizeSlider.value} x ${sizeSlider.value}`;
+
+sizeSlider.addEventListener(`input`, () => {
+    sizePara.textContent = `${sizeSlider.value} x ${sizeSlider.value}`;
+    container.textContent = ``;
+    createSquares(sizeSlider.value);
+    writeSquares();
+});
+
+createSquares(24);
+writeSquares();
 
 function createSquares(n) {
     for (let i = 1; i <= n * n; i++) {
@@ -22,16 +35,17 @@ function createSquares(n) {
     }
 };
 
-createSquares(24)
-
-let squares = document.querySelectorAll (`.square`);
-squares.forEach((square) => {
-    square.addEventListener(`mouseover`, (e) => {
-        if (e.buttons === 1) {    
+function writeSquares() {
+    let squares = document.querySelectorAll (`.square`);
+    squares.forEach((square) => {
+        square.addEventListener(`mouseover`, (e) => {
+            if (e.buttons === 1) {    
+                square.classList.add(`written`);
+            } else return;
+        })
+        square.addEventListener(`mousedown`, () => {
             square.classList.add(`written`);
-        } else return;
+        })
     })
-    square.addEventListener(`mousedown`, () => {
-        square.classList.add(`written`);
-    })
-})
+};
+    
